9298da9408b26a29c015a9a344279049b1e5a978
Scott Gigante <scottgigante@gmail.com>
2017-12-13 12:14:23 -0500

    add classical mds from https://github.com/KrishnaswamyLab/PHATE/blob/master/Python/tutorial/PHATE_tree.ipynb

--- python/mnn_impute/preprocessing.py
+++ python/mnn_impute/preprocessing.py
@@ -8,6 +8,55 @@ import scipy.sparse as sp
 from scipy.sparse.linalg import svds
 from sklearn.decomposition import RandomizedPCA
 
+# Classical MDS
+def cmdscale(D):
+    """
+    Classical multidimensional scaling (MDS)
+    Copyright © 2014-7 Francis Song, New York University
+    http://www.nervouscomputer.com/hfs/cmdscale-in-python/
+
+    Parameters
+    ----------
+    D : (n, n) array
+        Symmetric distance matrix.
+
+    Returns
+    -------
+    Y : (n, p) array
+        Configuration matrix. Each column represents a dimension. Only the
+        p dimensions corresponding to positive eigenvalues of B are returned.
+        Note that each dimension is only determined up to an overall sign,
+        corresponding to a reflection.
+
+    e : (n,) array
+        Eigenvalues of B.
+
+    """
+    # Number of points
+    n = len(D)
+
+    # Centering matrix
+    H = np.eye(n) - np.ones((n, n))/float(n)
+
+    # YY^T
+    B = -H.dot(D**2).dot(H)/2
+
+    # Diagonalize
+    evals, evecs = np.linalg.eigh(B)
+
+    # Sort by eigenvalue in descending order
+    idx   = np.argsort(evals)[::-1]
+    evals = evals[idx]
+    evecs = evecs[:,idx]
+
+    # Compute the coordinates using positive-eigenvalued components only
+    w, = np.where(evals > 0)
+    L  = np.diag(np.sqrt(evals[w]))
+    V  = evecs[:,w]
+    Y  = V.dot(L)
+
+    return Y, evals
+
 def svdpca(X, n_components, method='svd', verbose=False):
     """
     Mean-center and applies a number of SVD and PCA functions to data matrix X
