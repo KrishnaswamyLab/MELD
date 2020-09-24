901ba172e1cbf5c09cf336064f5b8d7d397054ba
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2020-09-24 17:08:24 -0400

    Update __init__.py

--- meld/__init__.py
+++ meld/__init__.py
@@ -4,3 +4,4 @@ from .meld import MELD
 from .cluster import VertexFrequencyCluster
 from .version import __version__
 from .benchmark import Benchmarker
+from .utils import get_meld_cmap as get_cmap
